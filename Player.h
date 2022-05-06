3bd2ebe5faa78bc5eee06af6478d0d33a9d362d1
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-05-06 13:04:22 +0300

    חתימות והגדרות של שחקן

---
+++ Player.h
@@ -0,0 +1,50 @@
+
+#ifndef MTM_HW2_PLAYER_H
+#define MTM_HW2_PLAYER_H
+
+#include "utilities.h"
+#include <string>
+
+typedef enum level {
+    ONE, TWO, THREE, FOR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN
+};
+
+class Player {
+    std::string m_name;
+    level m_level;
+    int m_force;
+    int m_maxHP;
+    int m_HP;
+    unsigned int m_coins;
+
+public:
+    Player(std::string m_name, int m_maxHP = 100, int force = 5);
+
+    Player(const Player &player) = default;
+
+    ~Player() = default;
+
+    Player &operator=(const Player &player) = default;
+
+    void printInfo();
+
+    void levelUp();
+
+    int getLevel();
+
+    void buff(int pointsToAdd);
+
+    void heal(int pointsToHeal);
+
+    void damage(int damagePoints);
+
+    bool isKnockedOut();
+
+    void addCoins(int coinsToAdd);
+
+    bool pay(int coinsToPay);
+
+    int getAttackStrength();
+};
+
+#endif //MTM_HW2_PLAYER_H

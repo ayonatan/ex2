9e359f6cc12efa2b4a56cf52e24ab84b39058186
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-05-06 15:54:52 +0300

    עדכון בנאי

--- Player.cpp
+++ Player.cpp
@@ -1,16 +1,27 @@
 #include "Player.h"
-
-Player::Player(std::string m_name, int m_maxHP = 100, int force = 5) {
-    if(illegal)
+#define DEFAULT_FORCE 5
+#define DEFAULT_MAX_HP 100
+Player::Player(std::string name, int maxHP = 100, int force = 5) :
+        m_level(ONE),
+        m_coins(0),
+        m_name(name) {
+    if (m_force <= 0)
     {
-        set to deafult vals;
+        m_force = DEFAULT_FORCE;
+    } else
+    {
+        m_force = force;
     }
-    this->m_coins = 0;
-    this->m_level = ONE;
-    this->m_force = force;
-    this->m_name = m_name;
-    this->m_HP = m_maxHP;
-    this->m_maxHP = m_maxHP;
+    if (maxHP <= 0)
+    {
+        this->m_HP = DEFAULT_MAX_HP;
+        this->m_maxHP = DEFAULT_MAX_HP;
+
+    } else
+    {
+        this->m_HP = maxHP;
+        this->m_maxHP = maxHP;
+    }
 }
 
 /*
@@ -55,8 +66,7 @@ void Player::buff(int pointsToAdd) {
 
 
 void Player::heal(int pointsToHeal) {
-    if (m_HP + pointsToHeal < m_maxHP)
-    {
+    if (m_HP + pointsToHeal < m_maxHP) {
         m_HP += pointsToHeal;
     } else {
         m_HP = m_maxHP;
@@ -64,37 +74,32 @@ void Player::heal(int pointsToHeal) {
 }
 
 void Player::damage(int damagePoints) {
-    if (m_HP-damagePoints>0)
-    {
-         m_HP -=damagePoints;
+    if (m_HP - damagePoints > 0) {
+        m_HP -= damagePoints;
     } else {
-       m_HP=0;
+        m_HP = 0;
     }
 }
 
-bool Player::isKnockedOut(){
-    if (m_HP<=0)
-    {
+bool Player::isKnockedOut() {
+    if (m_HP <= 0) {
         return true;
     }
     return false;
 }
 
-void Player::addCoins(int coinsToAdd)
-{
-    m_coins+=coinsToAdd;
+void Player::addCoins(int coinsToAdd) {
+    m_coins += coinsToAdd;
 }
 
-bool Player::pay(int coinsToPay)
-{
-    if (m_coins-coinsToPay<0)
-    {
+bool Player::pay(int coinsToPay) {
+    if (m_coins - coinsToPay < 0) {
         return false;
     }
-    m_coins-=coinsToPay;
+    m_coins -= coinsToPay;
     return true;
 }
 
-int Player::getAttackStrength(){
-    return m_level+m_force;
+int Player::getAttackStrength() {
+    return m_level + m_force;
 }
